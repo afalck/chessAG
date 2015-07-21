c6153d0b588fe0591e3005947344f13fa955e608
afalck <junk.afalck@gmail.com>
2015-07-21 06:59:30 -0400

    making sure GitHub is set up correctly

--- ChessAG/ViewController.swift
+++ ChessAG/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        println("adding a line to commit.");
+        println("adding a line to commit again.");
     }
 
     override func didReceiveMemoryWarning() {
